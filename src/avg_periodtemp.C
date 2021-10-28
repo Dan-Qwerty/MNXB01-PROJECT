e11c5293517126982f764b850c97f024230ac04b
Carl-Fredrik Lidgren <carl.fredrik.lidgren@gmail.com>
2021-10-28 02:48:40 +0200

    cleaned up the code a little, and temporarily changed the functionality

--- src/avg_periodtemp.C
+++ src/avg_periodtemp.C
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <iostream>
 
 // ROOT library objects
 #include <TF1.h> // 1d function class
@@ -22,11 +23,8 @@ int convert_dm_to_day(int day, int month){
     return days_prior + day;
 }
 
-void temperature_over_two_periods(Int_t year1a, Int_t year1b, Int_t year2a, Int_t year2b) {
-//                                  WeatherDataVec data) {
-	WeatherDataVec data(datavec("smhi-opendata_1_53430_20210926_101122_Lund.csv"));
-    auto period1Hist = new TH1D("period1Hist", "Period One Average Temperature; Day; Temperature", 365, 1, 365);
-    auto period2Hist = new TH1D("period2Hist", "Period Two Average Temperature; Day; Temperature", 365, 1, 365);
+TH1D* temperature_over_period(Int_t year1a, Int_t year1b, WeatherDataVec data){
+    auto hist = new TH1D("hist", "Average Temperature; Day; Temperature", 365, 1, 365);
 
    // histogram for the first period
     for(int day = 1; day <= 31; day++){
@@ -34,41 +32,27 @@ void temperature_over_two_periods(Int_t year1a, Int_t year1b, Int_t year2a, Int_
             if(day == 29 && month == 1)
                 continue; // skip the leap year problems
 
-            // compute the average
+            // compute the average over the average.
             std::vector<double> day_sums;
-            int total_measurements = 0;
             for(int year = year1a; year <= year1b; year++){
                 std::vector<double> daydata = data.get_by_year(year).get_by_day(day).get_by_month(month).list_temperatures();
-                double day_sum = std::accumulate(daydata.begin(), daydata.end(), 0.0);
-                total_measurements += daydata.size();
-                day_sums.push_back(day_sum);
+                double day_avg = std::accumulate(daydata.begin(), daydata.end(), 0.0)/daydata.size();
+                day_sums.push_back(day_avg);
             }
-            double day_avg = std::accumulate(day_sums.begin(), day_sums.end(), 0.0)/total_measurements;
+            double day_avg = std::accumulate(day_sums.begin(), day_sums.end(), 0.0)/day_sums.size();
 
-            period1Hist->SetBinContent(convert_dm_to_day(day, month), day_avg);
+            hist->SetBinContent(convert_dm_to_day(day, month), day_avg);
         }
     }
+	return hist;
+}
 
-    // histogram for the second period
-    for(int day = 1; day <= 31; day++){
-        for(int month = 1; month <= 12; month++){
-            if(day == 29 && month == 1)
-                continue; // skip the leap year problems
+void temperature_over_two_periods(Int_t year1a, Int_t year1b, Int_t year2a, Int_t year2b) {
+//                                  WeatherDataVec data) {
+	WeatherDataVec data(datavec("smhi-opendata_1_53430_20210926_101122_Lund.csv"));
+    auto period1Hist = temperature_over_period(year1a, year1b, data);
+    auto period2Hist = temperature_over_period(year2a, year2b, data);
 
-            // compute the average
-            std::vector<double> day_sums;
-            int total_measurements = 0;
-            for(int year = year2a; year <= year2b; year++){
-                std::vector<double> daydata = data.get_by_year(year).get_by_day(day).get_by_month(month).list_temperatures();
-                double day_sum = std::accumulate(daydata.begin(), daydata.end(), 0.0);
-                total_measurements += daydata.size();
-                day_sums.push_back(day_sum);
-            }
-            double day_avg = std::accumulate(day_sums.begin(), day_sums.end(), 0.0)/total_measurements;
-
-            period2Hist->SetBinContent(convert_dm_to_day(day, month), day_avg);
-        }
-    }
 
     auto c = new TCanvas("periodHist", "Average temperatures for two periods", 800, 800);
 	period1Hist->Draw();
